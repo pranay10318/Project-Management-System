1fa0273d03bfdd16e71be11347ac9caa98f33f51
pradharapu <pradharapu@demandbase.com>
2025-09-14 19:28:49 +0530

    Updates

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Project-Management-System/sw.js', { scope: '/Project-Management-System/' })})}
